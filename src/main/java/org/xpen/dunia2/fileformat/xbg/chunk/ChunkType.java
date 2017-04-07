52a521000afa94aeb0085503a1961098b124cb51
xpenxpen <xpenxpen@163.com>
2017-04-07 17:11:32 +0800

    add comment

--- src/main/java/org/xpen/dunia2/fileformat/xbg/chunk/ChunkType.java
+++ src/main/java/org/xpen/dunia2/fileformat/xbg/chunk/ChunkType.java
@@ -13,8 +13,8 @@ public class ChunkType {
     public static final int BOUNDING_BOX = 0x42424F58; // 'BBOX'
     public static final int BOUNDING_SPHERE = 0x42535048; // 'BSPH'
     public static final int LOD_INFO = 0x004C4F44; // 'LOD\0'
-    public static final int PCMP = 0x50434D50;
-    public static final int UCMP = 0x55434D50;
+    public static final int PCMP = 0x50434D50; // 'PCMP'
+    public static final int UCMP = 0x55434D50; // 'UCMP'
     public static final int MATERIAL_DESCRIPTOR = 0x444D544C; // 'DMTL'
 
     public static final int O2BM = 0x4F32424D; // 'O2BM'
