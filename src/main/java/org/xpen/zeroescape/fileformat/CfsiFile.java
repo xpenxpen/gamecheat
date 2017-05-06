9dcd4fee9d7ec20756dc92a486bf65a738606695
xpenxpen <xpenxpen@163.com>
2017-05-06 22:39:43 +0800

    zero escape 3 00000000 DONE

--- src/main/java/org/xpen/zeroescape/fileformat/CfsiFile.java
+++ src/main/java/org/xpen/zeroescape/fileformat/CfsiFile.java
@@ -24,7 +24,7 @@ public class CfsiFile {
     private RandomAccessFile raf;
     private FileChannel fileChannel;
     
-    private FatEntry optionalEntry;
+    //private FatEntry optionalEntry;
     private List<FolderEntry> folderEntries = new ArrayList<FolderEntry>();
     private String fileName;
     
@@ -38,13 +38,8 @@ public class CfsiFile {
     
     /**
      * CFSI File format
-     * 1 0x01 or 0xFC (if = 0xFC, read next 4~ bytes)
-     * 4 unknown (optional)
-     * 1 file name length
-     * X file name
-     * 4 unknown
-     * 4 length
-     * 
+     * 1 folder count 0x01 or 0xFC (if = 0xFC, read next 2 bytes)
+     * 2 folder count (optional)
      * 1 folder name length
      * X folder name
      * 1 entry count (if = 0xFC, read next 2 bytes)
@@ -53,7 +48,7 @@ public class CfsiFile {
      * |
      * |  LOOP ---1 file name length
      * |       |  X file name
-     * |       |  4 unknown
+     * |       |  4 start (*16)
      * |       ---4 length
      * |
      * |
@@ -82,113 +77,102 @@ public class CfsiFile {
         fileChannel.read(buffer);
         buffer.flip();
         
-        int firstByte = buffer.get();
+        int folderCount = buffer.get();
         //optional file
-        if (firstByte == -4) {
-            buffer = ByteBuffer.allocate(4);
-            buffer.order(ByteOrder.LITTLE_ENDIAN);
-            
-            fileChannel.read(buffer);
-            buffer.flip();
-        	
-            buffer = ByteBuffer.allocate(1);
-            buffer.order(ByteOrder.LITTLE_ENDIAN);
-            
-            fileChannel.read(buffer);
-            buffer.flip();
-            int length = buffer.get();
-            
-            buffer = ByteBuffer.allocate(length);
-            buffer.order(ByteOrder.LITTLE_ENDIAN);
-            
-            fileChannel.read(buffer);
-            buffer.flip();
-        	
-        	String optionalFileName = ByteBufferUtil.getFixedLengthString(buffer, length);
-        	
-            buffer = ByteBuffer.allocate(8);
-            buffer.order(ByteOrder.LITTLE_ENDIAN);
-            
-            fileChannel.read(buffer);
-            buffer.flip();
-            
-        	optionalEntry = new FatEntry();
-            optionalEntry.name = optionalFileName;
-            optionalEntry.unknown = buffer.getInt();
-            optionalEntry.length = buffer.getInt();
-            
-        }
-        
-    	FolderEntry folderEntry = new FolderEntry();
-    	folderEntries.add(folderEntry);
-    	
-        buffer = ByteBuffer.allocate(1);
-        buffer.order(ByteOrder.LITTLE_ENDIAN);
-        
-        fileChannel.read(buffer);
-        buffer.flip();
-        int length = buffer.get();
-        
-        buffer = ByteBuffer.allocate(length);
-        buffer.order(ByteOrder.LITTLE_ENDIAN);
-        
-        fileChannel.read(buffer);
-        buffer.flip();
-    	
-    	String folderName = ByteBufferUtil.getFixedLengthString(buffer, length);
-    	folderEntry.name = folderName;
-    	
-        buffer = ByteBuffer.allocate(1);
-        buffer.order(ByteOrder.LITTLE_ENDIAN);
-        
-        fileChannel.read(buffer);
-        buffer.flip();
-        int fileCount = buffer.get();
-        //more than 1 byte
-        if (fileCount == -4) {
+        if (folderCount == -4) {
             buffer = ByteBuffer.allocate(2);
             buffer.order(ByteOrder.LITTLE_ENDIAN);
             
             fileChannel.read(buffer);
             buffer.flip();
-        	fileCount = buffer.getShort();
+            folderCount = buffer.getShort();
         }
-        LOG.debug("fileCount={}", fileCount);
-        for (int j = 0; j < fileCount; j++) {
-        	FatEntry fatEntry = new FatEntry();
-        	folderEntry.fatEntries.add(fatEntry);
-        	
-            buffer = ByteBuffer.allocate(1);
-            buffer.order(ByteOrder.LITTLE_ENDIAN);
-            
-            fileChannel.read(buffer);
-            buffer.flip();
-            length = buffer.get();
-            
-            buffer = ByteBuffer.allocate(length);
-            buffer.order(ByteOrder.LITTLE_ENDIAN);
-            
-            fileChannel.read(buffer);
-            buffer.flip();
-        	
-        	String innerFileName = ByteBufferUtil.getFixedLengthString(buffer, length);
-            
-            buffer = ByteBuffer.allocate(8);
-            buffer.order(ByteOrder.LITTLE_ENDIAN);
-            
-            fileChannel.read(buffer);
-            buffer.flip();
-            
-            fatEntry.name = innerFileName;
-            fatEntry.unknown = buffer.getInt();
-            fatEntry.length = buffer.getInt();
-        	
+        
+        for (int i = 0; i < folderCount; i++) {
+        
+	    	FolderEntry folderEntry = new FolderEntry();
+	    	folderEntries.add(folderEntry);
+	    	
+	        buffer = ByteBuffer.allocate(1);
+	        buffer.order(ByteOrder.LITTLE_ENDIAN);
+	        
+	        fileChannel.read(buffer);
+	        buffer.flip();
+	        int length = buffer.get();
+	        
+	        String folderName = "";
+	        if (length != 0) {
+		        buffer = ByteBuffer.allocate(length);
+		        buffer.order(ByteOrder.LITTLE_ENDIAN);
+		        
+		        fileChannel.read(buffer);
+		        buffer.flip();
+		    	
+		        folderName = ByteBufferUtil.getFixedLengthString(buffer, length);
+	        }
+	    	folderEntry.name = folderName;
+	    	
+	        buffer = ByteBuffer.allocate(1);
+	        buffer.order(ByteOrder.LITTLE_ENDIAN);
+	        
+	        fileChannel.read(buffer);
+	        buffer.flip();
+	        int fileCount = buffer.get();
+	        //more than 1 byte
+	        if (fileCount == -4) {
+	            buffer = ByteBuffer.allocate(2);
+	            buffer.order(ByteOrder.LITTLE_ENDIAN);
+	            
+	            fileChannel.read(buffer);
+	            buffer.flip();
+	        	fileCount = buffer.getShort();
+	        }
+	        if (fileCount < 0) {
+	        	fileCount = fileCount + 256;
+	        }
+	        LOG.debug("pos={}, folderEntry.name={}, fileCount={}",
+	            fileChannel.position(), folderEntry.name, fileCount);
+	        for (int j = 0; j < fileCount; j++) {
+	        	FatEntry fatEntry = new FatEntry();
+	        	folderEntry.fatEntries.add(fatEntry);
+	        	
+	            buffer = ByteBuffer.allocate(1);
+	            buffer.order(ByteOrder.LITTLE_ENDIAN);
+	            
+	            fileChannel.read(buffer);
+	            buffer.flip();
+	            length = buffer.get();
+	            
+	            buffer = ByteBuffer.allocate(length);
+	            buffer.order(ByteOrder.LITTLE_ENDIAN);
+	            
+	            fileChannel.read(buffer);
+	            buffer.flip();
+	        	
+	        	String innerFileName = ByteBufferUtil.getFixedLengthString(buffer, length);
+	            
+	            buffer = ByteBuffer.allocate(8);
+	            buffer.order(ByteOrder.LITTLE_ENDIAN);
+	            
+	            fileChannel.read(buffer);
+	            buffer.flip();
+	            
+	            fatEntry.name = innerFileName;
+	            fatEntry.start = buffer.getInt() * 16;
+	            fatEntry.length = buffer.getInt();
+	        	
+	        }
         }
         
     }
 
     private void decodeDat() throws Exception {
+        LOG.debug("pos={}",
+	            fileChannel.position());
     	long previousPos = fileChannel.position();
+    	if (previousPos % 16 != 0) {
+    		previousPos = previousPos + 16 - previousPos % 16;
+    	}
     	
         for (int i = 0; i < folderEntries.size(); i++) {
         	FolderEntry folderEntry = folderEntries.get(i);
@@ -196,16 +180,17 @@ public class CfsiFile {
         	for (int j = 0; j < folderEntry.fatEntries.size(); j++) {
             	FatEntry fatEntry = folderEntry.fatEntries.get(j);
             	
-            	if (previousPos % 16 != 0) {
-            		previousPos = previousPos + 16 - previousPos % 16;
-            	}
-                raf.seek(previousPos);
-                LOG.debug("start={}, length={}", previousPos, fatEntry.length);
+//            	if (previousPos % 16 != 0) {
+//            		previousPos = previousPos + 16 - previousPos % 16;
+//            	}
+                raf.seek(previousPos + fatEntry.start);
+                LOG.debug("start={}, length={}, name={}, seek={}",
+                		previousPos, fatEntry.length, folderEntry.name + fatEntry.name, fatEntry.start);
                 
                 byte[] bytes = new byte[fatEntry.length];
                 raf.readFully(bytes);
                 
-                previousPos = previousPos + fatEntry.length;
+                //previousPos = previousPos + fatEntry.length;
 
                 File outFile = null;
                 outFile = new File(UserSetting.rootOutputFolder, fileName + "/" + folderEntry.name + fatEntry.name);
@@ -239,7 +224,7 @@ public class CfsiFile {
 
     public class FatEntry {
         public String name;
-        public int unknown;
+        public int start;
         public int length;
         
         @Override
